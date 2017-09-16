d96b57ad49275f39de413f44f7879fcfc600e5d9
Venkatesh Duppada <venkateshd1729@gmail.com>
2017-09-16 11:49:35 +0530

    Bump version: 0.0.2 → 0.1.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='EmoInt',
-    version='0.0.2',
+    version='0.1.0',
     description='Affective Computing',
     long_description=long_description,
     url='https://github.com/seernet/EmoInt',
